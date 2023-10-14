18f5b15407fd59e4d046f692c83ca202e48ac402
RVPdev <renatovilcahuaman@gmail.com>
2023-10-14 14:27:18 -0500

    seeting vscode

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Routes from "./Routes";
 
-function Layout () {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-            <Routes />
-        </div>
-    );
+function Layout() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Routes />
+    </div>
+  );
 }
 
 export default Layout;
